a59ebb610ada76910f8b78ee7b81e40c44d529fd
Michael Tommie McDuffie <mmcduffie@recomdata.com>
2012-07-09 13:31:13 -0400

    TMPSTGSQL-99 - Adding integration test for clinical data service.

---
+++ test/integration/com/recomdata/transmart/services/ClinicalDataServiceTests.groovy
@@ -0,0 +1,66 @@
+package com.recomdata.transmart.services
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+public class ClinicalDataServiceTests extends GroovyTestCase {
+	
+	def ClinicalDataService
+	
+	/**
+	 * This method will test the Clinical Data Export functionality for a specific patient list. 
+	 * In order to generate the "result_instance_id" used in this test I went into the interface and ran a workflow for the following;
+	 * Study : Breast_Cancer_Sorlie_GSE4382
+	 * Workflow : Scatter Plot
+	 * Independent Variable : \Sample Factors\End Points\Recurrence-Free Survival Time
+	 * Dependent Variable : \Sample Factors\End Points\Survival Time (Months)
+	 * 
+	 * After the test was done running I extracted the required parameters from the jobInfo.txt file that is created in the jobs temporary directory.
+	 */
+	void testDataRetrieval()
+	{
+		//This is a list of studies.		
+		def List studyList = ["GSE4382"];
+		
+		//This is a string array of concepts.
+		def String[] conceptCodeList = ['27','26'];
+		
+		//Create the file object to pass into the service. This is the directory where we write the output file.
+		def File studyDir = null
+		studyDir = new File("c:\\temp\\", "integrationTesting")
+		studyDir.mkdir()
+		
+		//Run the command to create the clinical data export file.
+		ClinicalDataService.getData(studyList,
+									studyDir,
+									"clinicalIntegrationText.txt",
+									"integrationTest",
+									"18886",
+									conceptCodeList,
+									["CLINICAL.TXT"],
+									false,
+									false,
+									[:],
+									"1",
+									[:],
+									[],
+									[] as String[],
+									false)
+		
+		//This is the file that should be created when we call the ClinicalDataService.
+		File dataFile = new File("c:\\temp\\integrationTesting\\Clinical\\clinicalIntegrationText.txt")
+
+		//Check for the existence of the exported data file.
+		assert dataFile.exists()
+		
+		//Count how many lines are in the file.
+		int num = 0;
+		dataFile.eachLine { num++ }
+		
+		//Check to see if the count is equal to what we expect it to be.
+		assert num == 234
+		
+	}
+
+}
