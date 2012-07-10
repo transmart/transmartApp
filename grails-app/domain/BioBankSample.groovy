d61dc0d484d1b5d726098c53f367429dd699b930
Michael Tommie McDuffie <mmcduffie@recomdata.com>
2012-07-10 08:57:19 -0400

    TMPSTGSQL-10 - Moving domain class over to PG.

--- grails-app/domain/BioBankSample.groovy
+++ grails-app/domain/BioBankSample.groovy
@@ -30,6 +30,7 @@ class BioBankSample
 	Date import_date
 	
 	static mapping = {
+		datasource 'dataSource_postgresql'
 		table 'BIOMART.BIOBANK_SAMPLE'
 		version false
 		id column:'SAMPLE_TUBE_ID'
