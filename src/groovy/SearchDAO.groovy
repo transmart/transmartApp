8c1f61aa81e46877d103a7afb26c57978bfdf2f9
Michael Tommie McDuffie <mmcduffie@recomdata.com>
2012-08-30 11:54:35 -0400

    PFIZER-97 - Data Access object for GWAS data.

---
+++ src/groovy/SearchDAO.groovy
@@ -0,0 +1,17 @@
+import org.apache.log4j.Logger
+import org.springframework.context.ApplicationContext
+
+class SearchDAO {
+	ApplicationContext ctx = org.codehaus.groovy.grails.web.context.ServletContextHolder.getServletContext().getAttribute(org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes.APPLICATION_CONTEXT)
+	def dataSource = ctx.getBean('dataSource')
+	def grailsApplication = ctx.getBean('grailsApplication')
+	
+	static Logger log = Logger.getLogger(SearchDAO.class)
+	
+	def getGwasData()
+	{
+		def results = bio.BioAssayAnalysisGwas.executeQuery("SELECT gwas.rsId,gwas.pValue,gwas.logPValue FROM bio.BioAssayAnalysisGwas gwas")
+		
+		return results
+	}
+}
