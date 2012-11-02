ce54c4e8a529454520f6f4408ae2c3d75bf3588a
John Boles <john.r.boles@gmail.com>
2012-11-02 12:00:59 -0400

    Test commit making sure the local development is working.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -17,9 +17,6 @@
  *
  ******************************************************************/
   
- 
-/** Test Commit **/
-
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
