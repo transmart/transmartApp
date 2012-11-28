aa0e34dd2cda68caf67b5995952cb1da1d18fd5b
mconser <mconser@WPRDUSMJFGPVG.wks.jnj.com>
2012-11-28 11:31:14 -0500

    JNJ-2528 and JNJ-2533, allow save to work on page 1 for edit/clone

--- grails-app/controllers/GeneSignatureController.groovy
+++ grails-app/controllers/GeneSignatureController.groovy
@@ -409,7 +409,7 @@ class GeneSignatureController {
 		// load real domain object, apply edit params from clone
 		def gsReal = GeneSignature.get(wizard.editId)
 		def origFile = gsReal.uploadFile
-		clone.copyPropertiesTo(gsReal)
+		clone.copyPropertiesTo(gsReal)		
 		gsReal.modifiedByAuthUser=user
 		gsReal.uploadFile = origFile
 
