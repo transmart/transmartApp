ec5f649ebec22ce029ec8da3c5f0a1c7de650bcd
Wayne Vetrone <wvetrone@gmail.com>
2012-12-04 14:24:35 -0500

    update the src info

--- src/java/com/recomdata/export/ExportColumn.java
+++ src/java/com/recomdata/export/ExportColumn.java
@@ -20,6 +20,7 @@
 
 package com.recomdata.export;
 
+import java.util.HashMap;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -35,12 +36,22 @@ public class ExportColumn {
 	private String label;
 	private String pattern;
 	private String type;
+	private Integer width;
 	
 	public ExportColumn(String id, String label, String pattern, String type) {
 		this.id = id;
 		this.label = label;
 		this.pattern = pattern;
 		this.type = type;
+		this.width = -1;
+	}
+	
+	public ExportColumn(String id, String label, String pattern, String type, Integer width) {
+		this.id = id;
+		this.label = label;
+		this.pattern = pattern;
+		this.type = type;
+		this.width = width;
 	}
 	
 	public JSONObject toJSONObject() throws JSONException {
@@ -49,6 +60,32 @@ public class ExportColumn {
 		json.put("header", label);
 		json.put("sortable", true);
 		json.put("width", 50);
+		return json;
+	}
+	
+	public JSONObject toJSON_DataTables() throws JSONException {
+		JSONObject json = new JSONObject();
+		json.put("sTitle", label);
+		
+		String typeOut;
+		if (type != null) {
+			if (type == "Number")
+				typeOut = "numeric";
+			else
+				typeOut = "string";
+			
+			json.put("sType", typeOut);
+		}
+		/*
+		if (width > 0)
+		{
+			json.put("sWidth", width.toString() + "px");
+		}
+		else
+		{
+			json.put("sWidth", "100px");
+		}*/
+
 		return json;
 	}
 
@@ -82,5 +119,13 @@ public class ExportColumn {
 
 	public void setType(String type) {
 		this.type = type;
+	}
+
+	public Integer getWidth() {
+		return width;
+	}
+
+	public void setWidth(Integer height) {
+		this.width = width;
 	}
 }
--- src/java/com/recomdata/util/IBioTag.java
+++ src/java/com/recomdata/util/IBioTag.java
@@ -16,9 +16,23 @@
  * 
  *
  ******************************************************************/
+  
 
-
+/**
+ * $Id: IExcelProfile.java 9178 2011-08-24 13:50:06Z mmcduffie $
+ * @author $Author: mmcduffie $
+ * @version $Revision: 9178 $
+ */
 package com.recomdata.util;
-public enum FolderType {
-	PROGRAM, STUDY, FOLDER, ANALYSIS, ASSAY
+
+import java.util.List;
+
+/** Interface that is implemented by domain objects that will be exported to Excel */
+public interface IBioTag {
+    /**
+     * Retrieves the object UID value
+     *
+     * @return a String of values
+     */
+    public String getBioUID();
 }
