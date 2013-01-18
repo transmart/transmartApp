8629260c7bc1267b5bb6a684d185988feb3c555d
unknown <mankapoor@USNTNMANKAPOOR1.us.deloitte.com>
2013-01-18 14:13:07 -0500

    Another fix for the 404 for chartservlet.css issue.

--- web-app/js/datasetExplorer/datasetExplorer.js
+++ web-app/js/datasetExplorer/datasetExplorer.js
@@ -4109,7 +4109,7 @@ function getAnalysisPanelContent()
 
 function printPreview(content)
 {
-	var stylesheet = "<html><head><link rel='stylesheet' type='text/css' href='css/chartservlet.css'></head><body>";
+	var stylesheet = "<html><head><link rel='stylesheet' type='text/css' href='../css/chartservlet.css'></head><body>";
 	var generator = window.open('', 'name', 'height=400,width=500, resizable=yes, scrollbars=yes');
 	var printbutton = "<input type='button' value=' Print this page 'onclick='window.print();return false;' />";
 	var savebutton = "<input type='button' value='Save'  onclick='document.execCommand(\"SaveAs\",null,\".html\")' />";
