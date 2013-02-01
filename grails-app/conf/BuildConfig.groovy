3a504e151f2a6bc37be2ba4791240136b0b29a7c
John Boles <john.r.boles@gmail.com>
2013-02-01 08:40:02 -0500

    Ensure that the name of the war file is transmart without the application number.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -20,6 +20,7 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.war.file = "transmart.war"
 
 //grails.plugin.location.rmodules = "C:\\git\\jnjtransmart\\Rmodules"
 
