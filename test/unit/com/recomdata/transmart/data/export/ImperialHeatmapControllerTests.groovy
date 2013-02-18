2958d73903cd3309a5e0c9be121d8ff2bf00cffb
Florian Guitton <f.guitton@imperial.ac.uk>
2013-02-18 18:32:42 +0000

    Controller + Service Adding

---
+++ test/unit/com/recomdata/transmart/data/export/ImperialHeatmapControllerTests.groovy
@@ -0,0 +1,17 @@
+package com.recomdata.transmart.data.export
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(ImperialHeatmapController)
+class ImperialHeatmapControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
