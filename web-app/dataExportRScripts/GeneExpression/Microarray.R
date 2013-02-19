561981ea0fc1f71f22c17c6c692d470c7de2b20e
Florian Guitton <f.guitton@imperial.ac.uk>
2013-02-19 15:44:58 +0000

    R calculation fix

--- web-app/dataExportRScripts/GeneExpression/Microarray.R
+++ web-app/dataExportRScripts/GeneExpression/Microarray.R
@@ -6,12 +6,13 @@ Data1 <- read.csv(File1, header = TRUE)
 Data2 <- read.csv(File2, header = TRUE)
 Mean1 <- rowSums(Data1[,2:ncol(Data1)])/(ncol(Data1)-1)
 Mean2 <- rowSums(Data2[,2:ncol(Data2)])/(ncol(Data2)-1)
-FC <- Mean2/Mean1
+
+FC <- 2^Mean2/2^Mean1
 Pval = 1
 i <- 1
 while (i < nrow(Data1)+1) {
-        Pval[i] <- t.test(Data1[i, 2:ncol(Data1)], Data2[i, 2:ncol(Data2)])$p.value;
-        i <- i+1
+	Pval[i] <- t.test(Data1[i, 2:ncol(Data1)], Data2[i, 2:ncol(Data2)])$p.value;
+	i <- i+1
 }
 Qval <- p.adjust(Pval, method="fdr", n=length(Pval))
 
@@ -23,16 +24,16 @@ colnames(Data)[ncol(Data)] = "q-value"
 
 i <- 1
 while (i < length(Qval) + 1) {
-        if ((Data[i,"FC"] > 1.1 || Data[i,"FC"] < 0.909091) && Data[i,"q-value"] < 0.9) {
-                Data <- rbind(Data, Data[i,])
-        }
-        i <- i+1
+	if ((Data[i,"FC"] > 1.5 || Data[i,"FC"] < 0.666667) && Data[i,"q-value"] < 0.05) {
+		Data <- rbind(Data, Data[i,])
+	}
+	i <- i+1
 }
 
 i <- 1
 while (i < length(Qval) + 1) {
-        Data <- Data[-1,]
-        i <- i+1
+	Data <- Data[-1,]
+	i <- i+1
 }
 
 unlink(File1, recursive = FALSE, force = TRUE)
