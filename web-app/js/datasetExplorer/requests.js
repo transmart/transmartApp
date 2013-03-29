c3a080d4a752b91be641b6b39c4e83c8599ec9ef
Gustavo Lopes <gustavo@thehyve.nl>
2013-03-29 09:58:24 +0100

    Whitespace

--- web-app/js/datasetExplorer/requests.js
+++ web-app/js/datasetExplorer/requests.js
@@ -233,37 +233,34 @@ return '<result_output_list>\
  		</ns4:request>\
  		</message_body>\
  		</ns6:request>';
-}    
+}
 
 
-function getCRCQueryRequest(subset, queryname)
-{
-if(queryname=="" || queryname==undefined)
-	{
-	var d=new Date();
-	queryname=GLOBAL.Username+"'s Query at "+ d.toString();
-	}
-var query=getCRCRequestHeader()+ '<user group="'+GLOBAL.ProjectID+'" login="'+GLOBAL.Username+'">'+GLOBAL.Username+'</user>\
+function getCRCQueryRequest(subset, queryname) {
+    if (queryname == "" || queryname == undefined) {
+        var d = new Date();
+        queryname = GLOBAL.Username + "'s Query at " + d.toString();
+    }
+
+    var query = getCRCRequestHeader() + '<user group="' + GLOBAL.ProjectID + '" login="' + GLOBAL.Username + '">' + GLOBAL.Username + '</user>\
             <patient_set_limit>0</patient_set_limit>\
             <estimated_time>0</estimated_time>\
             <request_type>CRC_QRY_runQueryInstance_fromQueryDefinition</request_type>\
         </ns4:psmheader>\
         <ns4:request xsi:type="ns4:query_definition_requestType" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">\
 				<query_definition>\
-                <query_name>'+queryname+'</query_name>\
+                <query_name>' + queryname + '</query_name>\
                 <specificity_scale>0</specificity_scale>';
 
-for(var i=1;i<=GLOBAL.NumOfQueryCriteriaGroups;i++)
-{
-var qcd=Ext.get("queryCriteriaDiv"+subset+'_'+i.toString());
-	if(qcd.dom.childNodes.length>0)
-	{
-	query=query+getCRCRequestPanel(qcd.dom, i);
-	}
-}
-query=query+getSecurityPanel()+"</query_definition>"+getCRCRequestFooter();
-//query=query+"</query_definition>"+getCRCRequestFooter();
-return query;
+    for (var i = 1; i <= GLOBAL.NumOfQueryCriteriaGroups; i++) {
+        var qcd = Ext.get("queryCriteriaDiv" + subset + '_' + i.toString());
+        if (qcd.dom.childNodes.length > 0) {
+            query = query + getCRCRequestPanel(qcd.dom, i);
+        }
+    }
+    query = query + getSecurityPanel() + "</query_definition>" + getCRCRequestFooter();
+
+    return query;
 }
 
 //takes actual dom element
