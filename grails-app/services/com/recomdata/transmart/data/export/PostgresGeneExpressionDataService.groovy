9c6dd492f4c8f93dcd8748be05e58e489db2c46f
Gustavo Lopes <gustavo@thehyve.nl>
2013-04-25 11:45:28 +0200

    Remove function call w/ unused result

--- grails-app/services/com/recomdata/transmart/data/export/PostgresGeneExpressionDataService.groovy
+++ grails-app/services/com/recomdata/transmart/data/export/PostgresGeneExpressionDataService.groovy
@@ -84,9 +84,6 @@ class PostgresGeneExpressionDataService {
 				//Create a query for the Subset.
 				if (null != resultInstanceId)
 				{
-					//Get the concepts for this result instance id.
-					def concepts = i2b2HelperService.getConcepts(resultInstanceId)
-		
 					//Add the subquery to the main query.
 					 sqlQuery = createMRNAHeatmapPathwayQuery(study, resultInstanceId, gplIds, pathway, timepoint, sampleTypes, tissueTypes)
 					 sampleQuery = createStudySampleAssayQuery(study,resultInstanceId, gplIds, timepoint, sampleTypes, tissueTypes )
