83226eacd347a659c4edf479ed9000ae44812c99
Gustavo Lopes <gustavo@thehyve.nl>
2013-05-31 12:38:21 +0200

    ws

--- grails-app/controllers/UserLandingController.groovy
+++ grails-app/controllers/UserLandingController.groovy
@@ -33,27 +33,27 @@ class UserLandingController {
 	 * Dependency injection for the springSecurityService.
 	 */
     def springSecurityService
-	
-	   def index = {
-            new AccessLog(username: springSecurityService.getPrincipal().username, event:"Login",
-                  eventmessage: request.getHeader("user-agent"),
-                  accesstime:new Date()).save()
-                  def skip_disclaimer = grailsApplication.config.com.recomdata?.skipdisclaimer?:false;
-                  if(skip_disclaimer){
-                        redirect(uri:'/search');     
-                  }else{
-                  redirect(uri: '/userLanding/disclaimer.gsp')
-                  }
-      }
-	def agree = {
-		new AccessLog(username: springSecurityService.getPrincipal().username, event:"Disclaimer accepted",
-			accesstime:new Date()).save()				
-		redirect(uri: '/search')
-	}
-	
-	def disagree = {
-		new AccessLog(username: springSecurityService.getPrincipal().username, event:"Disclaimer not accepted",			
-			accesstime:new Date()).save()
-	    redirect(uri: '/logout')
-	}
+
+    def index = {
+        new AccessLog(username: springSecurityService.getPrincipal().username, event:"Login",
+                eventmessage: request.getHeader("user-agent"),
+                accesstime:new Date()).save()
+        def skip_disclaimer = grailsApplication.config.com.recomdata?.skipdisclaimer?:false;
+        if(skip_disclaimer){
+            redirect(uri:'/search');
+        }else{
+            redirect(uri: '/userLanding/disclaimer.gsp')
+        }
+    }
+    def agree = {
+        new AccessLog(username: springSecurityService.getPrincipal().username, event:"Disclaimer accepted",
+                accesstime:new Date()).save()
+        redirect(uri: '/search')
+    }
+
+    def disagree = {
+        new AccessLog(username: springSecurityService.getPrincipal().username, event:"Disclaimer not accepted",
+                accesstime:new Date()).save()
+        redirect(uri: '/logout')
+    }
 }
