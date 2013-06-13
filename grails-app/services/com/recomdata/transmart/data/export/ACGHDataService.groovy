aa8b17b6a2b3d6aea644102f5998effbc2da279e
Ruslan Forostianov <ruslan@thehyve.nl>
2013-06-13 14:30:34 +0200

    [FT-193] Reduce waiting time for retrieving clinical + regions data, for aCGH analyses
    Implemented "no gorm" version of retrieving regions data

--- grails-app/services/com/recomdata/transmart/data/export/ACGHDataService.groovy
+++ grails-app/services/com/recomdata/transmart/data/export/ACGHDataService.groovy
@@ -1,18 +1,20 @@
 package com.recomdata.transmart.data.export
 
 import com.recomdata.transmart.data.export.util.FileWriterUtil
+import groovy.transform.CompileStatic
 import org.transmartproject.core.dataquery.acgh.ACGHValues
 import org.transmartproject.core.dataquery.acgh.RegionResult
 import org.transmartproject.core.dataquery.acgh.RegionRow
 import org.transmartproject.core.dataquery.assay.Assay
 import org.transmartproject.core.dataquery.constraints.ACGHRegionQuery
 import org.transmartproject.core.dataquery.constraints.CommonHighDimensionalQueryConstraints
+import org.transmartproject.core.dataquery.acgh.Region
 
 
 
 class ACGHDataService {
 
-    def dataQueryResourceService
+    def dataQueryResourceNoGormService
     def queriesResourceService
     def sessionFactory
 
@@ -20,8 +22,7 @@ class ACGHDataService {
                      File studyDir,
                      String fileName,
                      String jobName,
-                     resultInstanceId)
-    {
+                     resultInstanceId) {
         def q = new ACGHRegionQuery(
                 common: new CommonHighDimensionalQueryConstraints(
                         studies: [study],
@@ -38,7 +39,7 @@ class ACGHDataService {
             writerUtil = new FileWriterUtil(studyDir, fileName, jobName, 'aCGH',
                     null, "\t" as char)
             session = sessionFactory.openStatelessSession()
-            result = dataQueryResourceService.runACGHRegionQuery(q, session)
+            result = dataQueryResourceNoGormService.runACGHRegionQuery(q, session)
 
             doWithResult(result, writerUtil)
         } finally {
@@ -48,61 +49,66 @@ class ACGHDataService {
         }
     }
 
+    @CompileStatic
     private doWithResult(RegionResult regionResult,
                          FileWriterUtil writerUtil) {
 
         List<Assay> assays = regionResult.indicesList
-        def header = createHeader(assays)
+        String[] header = createHeader(assays)
         writerUtil.writeLine(header as String[])
 
-        def templateArray = new String[header.size() + 1] //+1 b/c 1st row has no header
+        def templateArray = new String[header.size() + 1]
+        //+1 b/c 1st row has no header
         Long i = 1; //for the first row
-        for (RegionRow row: regionResult.rows) {
-            def line = templateArray.clone()
-            def region = row.region
+        for (Iterator<RegionRow> iterator = regionResult.rows; ; iterator.hasNext()) {
+            RegionRow row = (RegionRow) iterator.next()
+            if (!row) {
+                break
+            }
+            String[] line = templateArray.clone()
+            Region region = row.region
 
-            line[0] = i++                   as String
-            line[1] = region.chromosome     as String
-            line[2] = region.start          as String
-            line[3] = region.end            as String
+            line[0] = i++ as String
+            line[1] = region.chromosome as String
+            line[2] = region.start as String
+            line[3] = region.end as String
             line[4] = region.numberOfProbes as String
 
             int j = 5
-            PER_ASSAY_COLUMNS.each { k, value ->
-                assays.each { assay ->
+            PER_ASSAY_COLUMNS.each {k, Closure<ACGHValues> value ->
+                assays.each {Assay assay ->
                     line[j++] = value(row.getRegionDataForAssay(assay)) as String
                 }
             }
 
             writerUtil.writeLine(line)
         }
-        writerUtil.finishWriting()
     }
 
-    private static final def PER_ASSAY_COLUMNS = [
-            chip:     { ACGHValues v -> v.getChipCopyNumberValue() },
-            flag:     { ACGHValues v -> v.getCopyNumberState().getIntValue() },
-            probloss: { ACGHValues v -> v.getProbabilityOfLoss() },
-            probnorm: { ACGHValues v -> v.getProbabilityOfNormal() },
-            probgain: { ACGHValues v -> v.getProbabilityOfGain() },
-            probamp:  { ACGHValues v -> v.getProbabilityOfAmplification() },
+    private static final Map PER_ASSAY_COLUMNS = [
+            chip: {ACGHValues v -> v.getChipCopyNumberValue()},
+            flag: {ACGHValues v -> v.getCopyNumberState().getIntValue()},
+            probloss: {ACGHValues v -> v.getProbabilityOfLoss()},
+            probnorm: {ACGHValues v -> v.getProbabilityOfNormal()},
+            probgain: {ACGHValues v -> v.getProbabilityOfGain()},
+            probamp: {ACGHValues v -> v.getProbabilityOfAmplification()},
     ]
 
-    private createHeader(List<Assay> assays) {
-        def r = [
+    private String[] createHeader(List<Assay> assays) {
+        List<String> r = [
                 'chromosome',
                 'start',
                 'end',
                 'num.probes',
         ];
 
-        PER_ASSAY_COLUMNS.keySet().each { head ->
-            assays.each { assay ->
+        PER_ASSAY_COLUMNS.keySet().each {String head ->
+            assays.each {Assay assay ->
                 /* FIXME: subjectId is not public */
-                r << "${head}.${assay.subjectId}"
+                r << "${head}.${assay.subjectId}".toString()
             }
         }
 
-        r
+        r.toArray(new String[r.size()])
     }
 }
