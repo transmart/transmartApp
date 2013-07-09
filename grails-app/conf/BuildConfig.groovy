59ebe3263358385588fa813e64f8cf494852589e
Gustavo Lopes <gustavo@thehyve.nl>
2013-07-09 17:17:41 +0200

    Use /public repository in thehyve's Nexus
    Instead of just /snapshots

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -40,14 +40,23 @@ grails.project.dependency.resolution = {
         mavenLocal()
         mavenCentral()
         mavenRepo([
-                name: 'repo.hyve.nl-snapshots',
-                root: 'http://repo.thehyve.nl/content/repositories/snapshots/',
+                name: 'repo.theyve.nl-public',
+                root: 'http://repo.thehyve.nl/content/repositories/public/',
         ])
     }
     dependencies {
 		runtime 'postgresql:postgresql:9.0-801.jdbc4'
 		compile 'antlr:antlr:2.7.7'
         compile 'org.transmartproject:transmart-core-api:1.0-SNAPSHOT'
+        compile('org.springframework.security.extensions:spring-security-saml2-core:1.0.0.RC3-f0fb87a') {
+            excludes 'spring-security-config', 'spring-security-core', 'spring-security-web', 'servlet-api'
+        }
+        /* update when grails spring-security-core uses a more recent version of spring-security */
+        runtime 'org.springframework.security:spring-security-config:3.0.7.RELEASE'
+		/*compile('org.opensaml:opensaml:2.6.0') {
+			excludes 'xml-apis', 'serializer', 'xercesImpl'
+		}
+		compile 'xerces:xercesImpl:2.10.0'*/
     }
 
     plugins {
