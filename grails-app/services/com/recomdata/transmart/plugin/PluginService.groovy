50b8cf14f12f36b9a51f3828facdb42472cd98b6
Gustavo Lopes <gustavo@thehyve.nl>
2013-07-09 17:20:23 +0200

    Fix code commented out wrong; add resources plugin hook

--- grails-app/services/com/recomdata/transmart/plugin/PluginService.groovy
+++ grails-app/services/com/recomdata/transmart/plugin/PluginService.groovy
@@ -27,6 +27,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.recomdata.plugins.PluginDescriptor
+import org.springframework.beans.factory.InitializingBean
 
 class PluginService {
 
