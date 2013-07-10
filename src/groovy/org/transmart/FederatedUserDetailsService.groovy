019ce5ba6fd9e074ab58fb14288ce8d9d96cb0bf
Gustavo Lopes <gustavo@thehyve.nl>
2013-07-10 14:42:40 +0200

    SAML Login: Auto-create inexistent users
    Added config options:
    org.transmart.security.saml.createInexistentUsers
    	"true" (string, not bool) to activate the feature
    org.transmart.security.saml.attribute.username
    org.transmart.security.saml.attribute.firstName
    org.transmart.security.saml.attribute.lastName
    org.transmart.security.saml.attribute.email
    	The attribute names to be searched in the assertion in order to get
    	the data with which to fill the new userʼs details

--- src/groovy/org/transmart/FederatedUserDetailsService.groovy
+++ src/groovy/org/transmart/FederatedUserDetailsService.groovy
@@ -1,13 +1,28 @@
 package org.transmart
 
 import com.recomdata.security.AuthUserDetailsService
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
+import org.hibernate.SessionFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.security.saml.SAMLCredential
 import org.springframework.security.saml.userdetails.SAMLUserDetailsService
+import org.springframework.transaction.TransactionStatus
+import org.transmart.searchapp.AuthUser
 
 // not in grails-app/services to avoid being automatically instantiated
 public class FederatedUserDetailsService implements SAMLUserDetailsService {
+
+    @Autowired
+    @Qualifier('grailsApplication')
+    def grailsApplication
+
+    @Autowired
+    SessionFactory sessionFactory
+
+    static Log log = LogFactory.getLog(FederatedUserDetailsService.class)
 
     @Autowired
     AuthUserDetailsService userDetailsService
@@ -15,14 +30,66 @@ public class FederatedUserDetailsService implements SAMLUserDetailsService {
 	@Override
 	public Object loadUserBySAML(SAMLCredential credential)
 			throws UsernameNotFoundException {
-        credential.getAttributeByName("urn:oid:0.9.2342.19200300.100.1.1")
 
-        //userDetailsService.loadUserByProperty('federatedId',
-        //        credential.nameID.value)
-        String federatedId = /*credential.nameID.value*/
-            credential.getAttributeByName("urn:oid:0.9.2342.19200300.100.1.1").
-                    attributeValues[0].value /* LDAP uid */
+        String federatedId = credential.nameID.value
+        try {
+            log.debug("Searching for user with federated id '$federatedId'")
+            return userDetailsService.loadUserByProperty('federatedId', federatedId, true)
+        } catch (UsernameNotFoundException nf) {
+            log.info("No user found with federated id '$federatedId")
+            tryCreateUser(credential, federatedId, nf)
+            log.info("Trying to load user with federated id '$federatedId' again")
 
-        userDetailsService.loadUserByProperty('federatedId', federatedId, true)
+            return userDetailsService.loadUserByProperty('federatedId', federatedId, true)
+        }
+
 	}
+
+    private void tryCreateUser(credential, federatedId, nf) {
+        if (grailsApplication.config.org.transmart.security.saml.createInexistentUsers != 'true') {
+            log.warn("Will not try to create user with federated id " +
+                    "'$federatedId', such option is deactivated")
+            throw nf
+        }
+
+        def getAttr = {
+            credential.getAttributeByName(it)?.attributeValues?.getAt(0)?.value
+        }
+        def attributes = grailsApplication.config.org.transmart.security.saml.attribute
+
+        AuthUser.withTransaction { TransactionStatus status ->
+            String username = null,
+                   realName = null,
+                   email    = null
+
+            if (attributes.username) {
+                username = getAttr(attributes.username)
+            }
+            if (attributes.firstName && attributes.lastName) {
+                realName = getAttr(attributes.firstName) ?: ''
+                if (realName) {
+                    realName += ' '
+                }
+                if (getAttr(attributes.lastName)) {
+                    realName += getAttr(attributes.lastName)
+                }
+            }
+            if (attributes.email) {
+                email = getAttr(attributes.email)
+            }
+
+            AuthUser newUser = AuthUser.createFederatedUser(federatedId,
+                    username, realName, email, sessionFactory.currentSession);
+            def outcome = newUser.save(flush: true)
+            if (outcome) {
+                log.info("Created new user. {federatedId=$federatedId, " +
+                        "username=$username, realName=$realName, email=" +
+                        "$email}")
+            } else {
+                log.error("Failed creating new user with federatedId " +
+                        "$federatedId, errors: " + newUser.errors)
+                throw nf
+            }
+        }
+    }
 }
