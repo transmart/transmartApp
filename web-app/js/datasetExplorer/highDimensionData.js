c8b750334576eca97ff7fedb13909792d048be51
Florian Guitton <f.guitton@imperial.ac.uk>
2013-08-08 17:05:32 +0100

    Removing SampleExplorer

--- web-app/js/datasetExplorer/highDimensionData.js
+++ web-app/js/datasetExplorer/highDimensionData.js
@@ -124,7 +124,7 @@ function determineHighDimVariableType(result){
 /**
  * read the result from heatmapvalidate call.
  * @param result
- * @param completedFunction
+ * @param divId
  */
 function readCohortData(result, divId)
 {
