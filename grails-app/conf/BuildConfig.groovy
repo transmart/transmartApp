dd4c8b50a385dd3d8ba678b878d888bac43df906
Ruslan Forostianov <ruslan@thehyve.nl>
2013-09-18 13:52:39 +0200

    Comment out code-coverage plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -109,7 +109,10 @@ grails.project.dependency.resolution = {
         runtime ":transmart-core:1.0-SNAPSHOT"
         runtime ":resources:1.2"
 
-        test ":code-coverage:1.2.6"
+        //This version of code coverage plugin does not work with grails 2.3
+        //Despite specified test scope declaration of this plugin appears in grails.xml of prod war
+        //cause ClassNotFoundationException: CodeCoverageGrailsPlugin
+        //test ":code-coverage:1.2.6"
     }
 }
 
