c5d5963b14180367d6c4da85820ac1b4140a7770
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2013-10-14 12:10:22 +0100

    small typo: a boolean remains in the Oracle SQL query

--- grails-app/services/GeneSignatureService.groovy
+++ grails-app/services/GeneSignatureService.groovy
@@ -585,7 +585,7 @@ public class GeneSignatureService {
 		results.each { countMap.put(it.getAt(0), it) }
 		*/
 
-		def permCriteria = (bAdmin) ? "(1=1)" : "(gs.CREATED_BY_AUTH_USER_ID="+userId+" or gs.PUBLIC_FLAG=TRUE)"
+		def permCriteria = (bAdmin) ? "(1=1)" : "(gs.CREATED_BY_AUTH_USER_ID="+userId+" or gs.PUBLIC_FLAG=1)"
 		StringBuffer nativeSQL = new StringBuffer();
 		nativeSQL.append("select gsi.SEARCH_GENE_SIGNATURE_ID as id, count(*) Gene_Ct, sum(CASE WHEN gsi.FOLD_CHG_METRIC>0 THEN 1 ELSE 0 END) Up_Ct, sum(CASE WHEN gsi.FOLD_CHG_METRIC<0 THEN 1 ELSE 0 END) Down_Ct ");
 		nativeSQL.append("from SEARCH_GENE_SIGNATURE_ITEM gsi join SEARCH_GENE_SIGNATURE gs on gsi.search_gene_signature_id=gs.search_gene_signature_id ");
