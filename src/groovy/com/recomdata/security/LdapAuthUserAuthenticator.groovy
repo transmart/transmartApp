f6bdaa04335a755485e5781f2bc16d8bdfffc515
Florian Guitton <f.guitton@imperial.ac.uk>
2013-10-15 19:06:59 +0100

    Adding Groovy Class for personalized authentication (for LDAP login)

---
+++ src/groovy/com/recomdata/security/LdapAuthUserAuthenticator.groovy
@@ -0,0 +1,11 @@
+package com.recomdata.security
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Florian
+ * Date: 15/10/13
+ * Time: 16:45
+ * To change this template use File | Settings | File Templates.
+ */
+class LdapAuthUserAuthenticator {
+}
---
+++ src/groovy/com/recomdata/security/LdapAuthUserDetailsMapper.java
@@ -0,0 +1,11 @@
+package com.recomdata.security;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Florian
+ * Date: 14/10/13
+ * Time: 17:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class LdapAuthUserDetailsMapper {
+}
