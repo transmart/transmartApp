e65c88277819b39de1d7a5da37ad35699223a196
temoedjin <temoedjin@gmail.com>
2013-11-29 12:21:26 +0100

    refactor RModules.knownDataTypes method plus parse JSON in a grails way

--- test/integration/com/recomdata/transmart/data/association/RModulesControllerTests.groovy
+++ test/integration/com/recomdata/transmart/data/association/RModulesControllerTests.groovy
@@ -1,15 +1,14 @@
 package com.recomdata.transmart.data.association
 
 import grails.test.mixin.TestFor
-import groovy.json.JsonSlurper
+import grails.converters.JSON
 
 @TestFor(RModulesController)
 class RModulesControllerTests {
 
     void "test that knownDataTypes gives back expected output for mrna"() {
         controller.knownDataTypes()
-        JsonSlurper jsonSlurper = new JsonSlurper()
-		Object result = jsonSlurper.parseText(controller.response.contentAsString)
+        Object result = JSON.parse(controller.response.contentAsString)
         assert result.size() >= 3
         assert result["mrna"] != null
         assert result["mrna"].dataConstraints.size() > 0
@@ -19,8 +18,7 @@ class RModulesControllerTests {
 
     void "test that knownDataTypes gives back expected output for mirna"() {
         controller.knownDataTypes()
-        JsonSlurper jsonSlurper = new JsonSlurper()
-		Object result = jsonSlurper.parseText(controller.response.contentAsString)
+		Object result = JSON.parse(controller.response.contentAsString)
         assert result.size() >= 3
         assert result["mirna"] != null
         assert result["mirna"].dataConstraints.size() > 0
@@ -30,8 +28,7 @@ class RModulesControllerTests {
 
     void "test that knownDataTypes gives back expected output for acgh"() {
         controller.knownDataTypes()
-        JsonSlurper jsonSlurper = new JsonSlurper()
-		Object result = jsonSlurper.parseText(controller.response.contentAsString)
+        Object result = JSON.parse(controller.response.contentAsString)
         assert result.size() >= 3
         assert result["acgh"] != null
         assert result["acgh"].dataConstraints.size() > 0
