74875deab1dbf79b1342cc6e3f0d22db034906f9
Peter <peter@thehyve.nl>
2014-01-24 16:57:46 +0100

    Adds metabolite support to high dimensional data pop-up.

--- grails-app/controllers/SearchController.groovy
+++ grails-app/controllers/SearchController.groovy
@@ -76,7 +76,7 @@ public class SearchController{
      * - GENESIG
      */
     def loadSearchPathways = {
-        params.query = 'protein'+SEARCH_DELIMITER+'mirna'+SEARCH_DELIMITER+'gene'+SEARCH_DELIMITER+'pathway'+SEARCH_DELIMITER+'genelist'+SEARCH_DELIMITER+'genesig:'+params.query
+        params.query = 'metabolite'+SEARCH_DELIMITER+'protein'+SEARCH_DELIMITER+'mirna'+SEARCH_DELIMITER+'gene'+SEARCH_DELIMITER+'pathway'+SEARCH_DELIMITER+'genelist'+SEARCH_DELIMITER+'genesig:'+params.query
         loadSearch()
     }
 
