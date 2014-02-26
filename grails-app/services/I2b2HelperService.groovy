transmart/transmartApp

commit 30b83592d9f5ee9942fadfbe4ab3a7cf1a3ab557

[UAT-166] Backend fixes for grid view issues
- Remove unused logic for disambiguate facts
- Give more ext.js-frienly ids for data cells

grails-app/services/I2b2HelperService.groovy
@@ -719,9 +719,8 @@ class I2b2HelperService {
      */
     def ExportTableNew addConceptDataToTable(ExportTableNew tablein, String concept_key, String result_instance_id) {
         if (isLeafConceptKey(concept_key)) {
-            String columnid = getShortNameFromKey(concept_key).replace(" ", "_").replace("...", "").replace(".", "");
+            String columnid = "col_${tablein.columns.size()}"
             String columnname = getColumnNameFromKey(concept_key).replace(" ", "_");
-            //String columnid="...test\\test";
             /*add the column to the table if its not there*/
             if (tablein.getColumn("subject") == null) {
                 tablein.putColumn("subject", new ExportColumn("subject", "Subject", "", "string"));
@@ -842,7 +841,7 @@ class I2b2HelperService {
 		}
 		
 		// After that, retrieve all data entries for the children
-		def results = ObservationFact.executeQuery( "SELECT o.patient.id, o.textValue FROM ObservationFact o WHERE ( modifierCd = '@' OR modifierCd = sourcesystemCd ) AND conceptCode IN (:conceptCodes) AND o.patient.id in (:patientNums)", [ conceptCodes: concepts*.conceptCode, patientNums: patientIds.collect { it?.toLong() } ] )
+		def results = ObservationFact.executeQuery( "SELECT o.patient.id, o.textValue FROM ObservationFact o WHERE conceptCode IN (:conceptCodes) AND o.patient.id in (:patientNums)", [ conceptCodes: concepts*.conceptCode, patientNums: patientIds.collect { it?.toLong() } ] )
 
 		results.each { row ->
 	

commit 6b60df072c6bc2808b359464b77ede1af957de4b

Fix two columns added in grid view when dragging 1
when we have two subsets.

addConceptDataToTable() is called for each subset and it adds a column
to the table. However, if the concept key is the same, a colum with the
same id used to be added, overwriting the previous one (but keeping its
data). This was broken in 30b83592, where the column ids started being
generated sequentially. The idea there was to generate ids that didn't
cause trouble to extjs, but it went too far.

Changed to code to generated ids by calculating a checksum on the
concept key.

Fixes UAT-181.

grails-app/services/I2b2HelperService.groovy
@@ -719,7 +719,7 @@ class I2b2HelperService {
      */
     def ExportTableNew addConceptDataToTable(ExportTableNew tablein, String concept_key, String result_instance_id) {
         if (isLeafConceptKey(concept_key)) {
-            String columnid = "col_${tablein.columns.size()}"
+            String columnid = concept_key.encodeAsSHA1()
             String columnname = getColumnNameFromKey(concept_key).replace(" ", "_");
             /*add the column to the table if its not there*/
             if (tablein.getColumn("subject") == null) {