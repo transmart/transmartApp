bb8b65c649699a3dd474f7f21cfee77260ce45ff
Peter Rice <p.rice@imperial.ac.uk>
2014-06-09 21:25:10 +0100

    Logistic regression missing function

--- web-app/js/datasetExplorer/i2b2common.js
+++ web-app/js/datasetExplorer/i2b2common.js
@@ -1775,6 +1775,17 @@ if(!this.compareStepPathwaySelectionRBM)
                
 }
 
+function getQueryCdItem(el){
+    var item=el.getAttribute("conceptid");
+
+    var inOutCode = el.getAttribute("inoutcode");
+
+    //If there is visit information in the node, add it to the item we return.
+    if(inOutCode) item += "~" + inOutCode;
+
+    return item;
+}
+
 function getQuerySummary(subset)
 {
 var query="";
