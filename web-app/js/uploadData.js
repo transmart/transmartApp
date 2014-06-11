2abaec536c3ec50075183b377bc86810643781fc
Charlotte Raillere <charlotte.raillere@sanofi.com>
2014-06-11 13:09:20 +0200

    Fix autocompletion in Browse object creation by moving functions related to metadata in a separate file

--- web-app/js/uploadData.js
+++ web-app/js/uploadData.js
@@ -165,42 +165,6 @@ function removeTag(fieldName, tag) {
     $j('#' + escapedFieldName + ' option[value="' + tag + '"]').remove();
 }
 
-function addPlatform(field) {
-	//Gather the relevant elements
-	var typeFieldName = '#' + field + 'Name';
-	var typeField = $j(typeFieldName);
-	var vendorField = $j('#' + field + 'Vendor');
-	var selectField = $j('#' + field);
-	var tagDiv = $j('#' + field + '-tags');
-
-	//If we have a non-null platform ID, prepare to add it
-	var platformId = typeField.val();
-	var platformName = $j(typeFieldName + " option:selected").text()
-	if (platformId == null || platformId == "null") { return; }
-	
-	//Stop if we already have this tag
-	var existingPlatform = $j('option[value="' + platformId + '"]', selectField);
-	if (existingPlatform.length > 0) { typeField.val(null); return; }
-	
-	//Get text and value to add
-	var valueToAdd = platformId;
-	var textToAdd = vendorField.val() + ": " + platformName;
-	//Now add to the hidden select
-	selectField.append($j('<option></option>').val(valueToAdd).text(textToAdd).attr('selected', 'selected'));
-	
-	//Create and add the visible tag to the page
-	var newTag = $j('<span/>', {
-		id: field + '-tag-' + valueToAdd,
-		'class': 'tag',
-		name: valueToAdd
-	}).text(textToAdd);
-	
-	$j('#' + field + '-tags').append(newTag);
-	newTag.hide().fadeIn('slow');
-	typeField.val(null);
-	
-}
-
 function fillSelectAjax(element, url, params) {
 	
 	request = $j.ajax({
@@ -310,14 +274,6 @@ function downloadTemplate() {
 	else
 		window.location = templateDownloadUrl + "?type=" + type;
 }
-
-function loadPlatformTypes(field, type) {
-	var targetField = $j('#' + field + 'Name');
-	var sourceField = $j('#' + field + 'Vendor');
-	var vendor = sourceField.val();
-	fillSelectAjax(targetField, platformTypesUrl, {vendor:vendor, type:type});
-}
-
 
 //After the user clicks select on the study popup, transfer the study ID and title and update the study table
 function applyStudyBrowse() {
