5d75051f2d8b2921f31cba317ca27bcf87fc38d9
forus <foro.ru@gmail.com>
2014-06-29 09:13:52 +0200

    Add transmart-rest-api to plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -98,6 +98,7 @@ grails.project.dependency.resolution = {
         compile ':transmart-gwas:1.1-SNAPSHOT'
         runtime ':dalliance-plugin:0.2'
         runtime ':transmart-mydas:0.1-SNAPSHOT'
+        runtime ':transmart-rest-api:0.1-SNAPSHOT'
         //runtime ':blend4j-plugin:0.1-SNAPSHOT'
 
         // Doesn't work with forked tests yet
