79afef597f099c13a1730c00a779b3b0492bb92b
Charlotte Raillere <charlotte.raillere@sanofi.com>
2014-07-18 14:04:14 +0200

    Fix gene signature clone or edit (TMART-207)

--- grails-app/controllers/GeneSignatureController.groovy
+++ grails-app/controllers/GeneSignatureController.groovy
@@ -145,7 +145,7 @@ class GeneSignatureController {
         def geneSigInst = GeneSignature.get(params.id)
         def clone = geneSigInst.clone()
         clone.modifiedByAuthUser = user
-        if (clone.experimentTypeCellLine.id == null) clone.experimentTypeCellLine = null     // this is hack, don't know how to get around this!
+        if (clone.experimentTypeCellLine?.id == null) clone.experimentTypeCellLine = null     // this is hack, don't know how to get around this!
         log.debug "experimentTypeCellLine: " + clone.experimentTypeCellLine + "; null? " + (clone.experimentTypeCellLine == null)
 
         // set onto session
@@ -175,7 +175,7 @@ class GeneSignatureController {
         clone.lastUpdated = null;
         clone.versionNumber = null;
         clone.uniqueId = null;
-        if (clone.experimentTypeCellLine.id == null) clone.experimentTypeCellLine = null     // this is hack, don't know how to get around this!
+        if (clone.experimentTypeCellLine?.id == null) clone.experimentTypeCellLine = null     // this is hack, don't know how to get around this!
 
         // set onto session
         def newWizard = new WizardModelDetails(loggedInUser: user, geneSigInst: clone, wizardType: WizardModelDetails.WIZ_TYPE_CLONE, cloneId: geneSigInst.id);
