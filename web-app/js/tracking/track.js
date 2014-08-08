f5daf3513b4d6ee4eb9e43b4042283bcd545cc3c
Terry Weymouth <weymouth@umich.edu>
2014-08-08 14:06:32 -0400

    Added Google Analytics on top of tracking framework

--- web-app/js/tracking/track.js
+++ web-app/js/tracking/track.js
@@ -2,4 +2,12 @@
 	Include here, tracking scripts, such as Google Analytics
 */
 // brute-force test for including tracking (on each page)
-alert("Tracking script");
+// alert("Tracking script");
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-53648820-1', 'auto');
+  ga('send', 'pageview');
