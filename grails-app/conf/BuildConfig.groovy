7ac1fc6c2ee607b5e4303bb34576b694d4d8a031
Florian Guitton <f.guitton@imperial.ac.uk>
2015-04-10 13:25:21 +0100

    Little version uplift and .gitignore update

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -109,8 +109,8 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build ':release:3.0.1'
-        build ':rest-client-builder:2.0.1'
+        build ':release:3.1.1'
+        build ':rest-client-builder:2.1.1'
         build ':tomcat:7.0.52.1'
 
         compile ':hibernate:3.6.10.10'
