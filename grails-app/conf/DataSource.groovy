245fe90f7ac1eb94a381ef31ca8af176400e46cf
Florian Guitton <f.guitton@imperial.ac.uk>
2015-06-15 17:31:13 +0100

    Bypassing cache naming duplication problem at boot time by using a different EhCache factory

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -36,7 +36,7 @@ environments {
         hibernate {
             cache.use_second_level_cache = true
             cache.use_query_cache = false
-            cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+            cache.region.factory_class = 'net.sf.ehcache.hibernate.SingletonEhCacheRegionFactory'
         }
     }
 }
