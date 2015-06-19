9d6917c1ffb9603b6aae9332a56c82b36581038a
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2015-06-19 16:56:25 +0200

    Upgrade oauth & spring security modules (again)

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -220,30 +220,8 @@ log4j = {
     }
 }
 
-// Uncomment and edit the following lines to start using Grails encoding & escaping improvements
-
-/* remove this line
-// GSP settings
-grails {
-    views {
-        gsp {
-            encoding = 'UTF-8'
-            htmlcodec = 'xml' // use xml escaping instead of HTML4 escaping
-            codecs {
-                expression = 'html' // escapes values inside null
-                scriptlet = 'none' // escapes output from scriptlets in GSPs
-                taglib = 'none' // escapes output from taglibs
-                staticparts = 'none' // escapes output from static template parts
-            }
-        }
-        // escapes all not-encoded output at final stage of outputting
-        filteringCodecForContentType {
-            //'text/html' = 'html'
-        }
-    }
-}
-remove this line */
-
-/*
-// MetaCore plugin
-com.thomsonreuters.transmart.metacoreAnalyticsEnable=true */
+// Added by the Spring Security OAuth2 Provider plugin:
+grails.plugin.springsecurity.oauthProvider.clientLookup.className = 'org.transmart.oauth2.Client'
+grails.plugin.springsecurity.oauthProvider.authorizationCodeLookup.className = 'org.transmart.oauth2.AuthorizationCode'
+grails.plugin.springsecurity.oauthProvider.accessTokenLookup.className = 'org.transmart.oauth2.AccessToken'
+grails.plugin.springsecurity.oauthProvider.refreshTokenLookup.className = 'org.transmart.oauth2.RefreshToken'
--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,5 +1,16 @@
 //check external configuration as described in Config.groovy
 
+
+dataSource_oauth2 {
+    driverClassName = 'org.h2.Driver'
+    url = "jdbc:h2:~/.grails/oauth2db;MVCC=TRUE"
+    username = 'sa'
+    password = ''
+    dbCreate = 'update'
+    logSql = true
+    formatSql = true
+}
+
 environments {
     test {
         dataSource {
@@ -11,6 +22,17 @@ environments {
             logSql = true
             formatSql = true
         }
+
+        dataSource_oauth2 {
+            driverClassName = 'org.h2.Driver'
+            url = "jdbc:h2:mem:oauth2;MVCC=TRUE"
+            username = 'sa'
+            password = ''
+            dbCreate = 'update'
+            logSql = true
+            formatSql = true
+        }
+
         hibernate {
             cache.use_second_level_cache = true
             cache.use_query_cache = false
