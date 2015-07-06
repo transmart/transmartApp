ce3306369667033aa06229da0b09df16ad0ab610
Florian Guitton <f.guitton@imperial.ac.uk>
2015-07-06 11:07:37 +0100

    Correcting to suite upstream fixes in master branch

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -45,7 +45,6 @@ environments {
         hibernate {
             cache.use_second_level_cache = true
             cache.use_query_cache = false
-            cache.region.factory_class = 'net.sf.ehcache.hibernate.SingletonEhCacheRegionFactory'
         }
     }
 }
