2fab636af12f81e597a579566266a75f998f2c4d
Ruslan Forostianov <ruslan@thehyve.nl>
2015-12-24 12:41:51 +0100

    Revert "Making sure not to comflict with the mangle check operated by the RModule. This will entail usernames being similar but special caractere to be equal. Just bear that in mind. However we do it because it doesn't break the RModule way of dealing with it."
    This reverts commit bbf952e118c64cacb58aa998a5c018d14bbd2944.

--- grails-app/services/com/recomdata/transmart/data/export/ExportService.groovy
+++ grails-app/services/com/recomdata/transmart/data/export/ExportService.groovy
@@ -35,7 +35,7 @@ class ExportService {
         newJob.jobStatus = jobStatus
         newJob.save()
 
-        def jobName = userName.replaceAll(/[^0-9A-Za-z]*/, "") + "-" + analysis + "-" + newJob.id
+        def jobName = userName + "-" + analysis + "-" + newJob.id
         newJob.jobName = jobName
         newJob.altViewerURL = 'Test'
         newJob.save()
