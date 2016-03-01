1ab7754835224677c18699f6eae0337daacac278
Gijs Kant <gijs@thehyve.nl>
2016-03-01 15:54:06 +0100

    Update of audit logging filters.

--- grails-app/conf/org/transmartproject/app/AuditLogFilters.groovy
+++ grails-app/conf/org/transmartproject/app/AuditLogFilters.groovy
@@ -1,10 +1,11 @@
 package org.transmartproject.app
 
 import org.transmartproject.core.users.User
+import org.transmartproject.core.log.AccessLogEntryResource
 
 class AuditLogFilters {
 
-    def accessLogService
+    AccessLogEntryResource accessLogService
     def auditLogService
     def studyIdService
     User currentUserBean
@@ -18,12 +19,11 @@ class AuditLogFilters {
                 def result_instance_id2 = params.result_instance_id2 ?: ''
                 def studies = studyIdService.getStudyIdsForQueries([result_instance_id1, result_instance_id2])
 
-                accessLogService.report(currentUserBean, 'Data Export',
+                accessLogService?.report(currentUserBean, 'Data Export',
                     eventMessage: "User (IP: ${ip}) requested export of data. Http request parameters: ${params}",
                     requestURL: fullUrl)
-                auditLogService.report("Data Export", request,
+                auditLogService?.report("Data Export", request,
                     study: studies,
-                    url: fullUrl as String,
                     user: currentUserBean,
                 )
             }
@@ -40,35 +40,36 @@ class AuditLogFilters {
                 if (studies.empty) {
                     studies = studyIdService.getStudyIdsForQueries([result_instance_id1, result_instance_id2])
                 }
-                def task = "Summary Statistics (${actionName})"
-                auditLogService.report(task, request,
+                def task = "Summary Statistics"
+                if (actionName == "childConceptPatientCounts") {
+                    task = "Clinical Data Access"
+                }
+                auditLogService?.report(task, request,
                         study: studies,
                         user: currentUserBean,
                         subset1: result_instance_id1,
-                        subset2: result_instance_id2,
+                        subset2: result_instance_id2
                 )
             }
         }
         chart(controller: 'RWG', action: 'getFacetResults') {
             before = { model ->
-                auditLogService.report("Clinical Data Active Filter", request,
-                        action: params.searchString,
+                auditLogService?.report("Clinical Data Active Filter", request,
+                        query: params.searchTerms,
                         user: currentUserBean,
                 )
             }
         }
         userlanding(controller: 'userLanding', action: '*', actionExclude:'checkHeartBeat') {
             before = { model ->
-                auditLogService.report("User Access", request,
-                        action: actionName,
+                auditLogService?.report("User Access", request,
                         user: currentUserBean,
                 )
             }
         }
         oauth(controller: 'oauth', action: '*') {
             before = { model ->
-                auditLogService.report("OAuth authentication", request,
-                        action: actionName,
+                auditLogService?.report("OAuth authentication", request,
                         user: currentUserBean,
                 )
             }
