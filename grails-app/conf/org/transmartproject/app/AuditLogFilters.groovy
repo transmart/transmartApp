b0bd4f73a0939cc2f072f6992f51751c9fe850cf
Gijs Kant <gijs@thehyve.nl>
2016-03-15 12:43:37 +0100

    Distinguish LDD and HDD exports in audit logging.

--- grails-app/conf/org/transmartproject/app/AuditLogFilters.groovy
+++ grails-app/conf/org/transmartproject/app/AuditLogFilters.groovy
@@ -1,5 +1,7 @@
 package org.transmartproject.app
 
+import grails.converters.JSON
+import org.codehaus.groovy.grails.web.json.*
 import org.transmartproject.core.users.User
 import org.transmartproject.core.log.AccessLogEntryResource
 
@@ -10,6 +12,31 @@ class AuditLogFilters {
     def studyIdService
     User currentUserBean
 
+    /**
+     * Get export types from the <var>selectedSubsetDataTypeFiles</var>
+     * field in the parameters.
+     * Returns a list containing 'LDD' if the parameter contains data type 'CLINICAL'
+     * and 'HDD' if any other data type is in the parameter.
+     *
+     * @param params
+     * @return a list containing 'LDD', 'HDD', or both.
+     */
+    private List<String> getExportTypes(params) {
+        def param = params.selectedSubsetDataTypeFiles
+        def dataFiles = param instanceof String ?
+                [JSON.parse(param)] :
+                param.collect { JSON.parse(it) }
+        Set<String> dataTypes = dataFiles*.dataTypeId as Set
+        List<String> exportTypes = []
+        if ('CLINICAL' in dataTypes) {
+            exportTypes += 'LDD'
+        }
+        if ((dataTypes - 'CLINICAL').size() > 0) {
+            exportTypes += 'HDD'
+        }
+        exportTypes
+    }
+
     def filters = {
         dataExport(controller: 'dataExport', action:'runDataExport') {
             after = { model ->
@@ -18,13 +45,14 @@ class AuditLogFilters {
                 def result_instance_id1 = params.result_instance_id1 ?: ''
                 def result_instance_id2 = params.result_instance_id2 ?: ''
                 def studies = studyIdService.getStudyIdsForQueries([result_instance_id1, result_instance_id2])
+                def exportTypes = getExportTypes(params).join('+')
 
                 accessLogService?.report(currentUserBean, 'Data Export',
                     eventMessage: "User (IP: ${ip}) requested export of data. Http request parameters: ${params}",
                     requestURL: fullUrl)
-                auditLogService?.report("Data Export", request,
+                auditLogService?.report("Clinical Data Exported - ${exportTypes}", request,
                     study: studies,
-                    user: currentUserBean,
+                    user: currentUserBean
                 )
             }
         }
