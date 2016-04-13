fd034fd57bff6b382f7bdbaffab54b1bd9b9a70a
Jan Kanis <jan.code@jankanis.nl>
2016-04-13 15:24:10 +0200

    pfizer_audit_metrics.py: handle missing/unknown browser

--- demo/pfizer_audit_logger.py
+++ demo/pfizer_audit_logger.py
@@ -52,14 +52,17 @@ def send_auditlog_record(line):
         action = user
     else:
         action = msg.get('action') or '|'.join(msg.get(x) for x in ('study', 'subset1', 'subset2', 'analysis', 'query', 'facetQuery', 'clientId') if msg.get(x))
-    browser = httpagentparser.detect(msg['userAgent'])['browser']
     args = dict(action = action,
                 application = msg['program'],
                 appVersion = msg['programVersion'],
                 user = user,
                 task = task,
-                browser = browser['name'] + ' ' + browser['version']
             )
+    if msg['userAgent']:
+        args['browser'] = '<unknown browser>'
+        browser = httpagentparser.detect(msg['userAgent']).get('browser')
+        if browser:
+            args['browser'] = browser['name'] + ' ' + browser['version']
     fullurl = URL + '?' + urllib.parse.urlencode(args)
     #print(fullurl)
     try:
