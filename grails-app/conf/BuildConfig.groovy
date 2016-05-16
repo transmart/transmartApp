b7a1baf8e954375937e766645d137d7498000442
Terry Weymouth <weymouth@umich.edu>
2016-05-16 15:00:00 -0400

    update version number to 16.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -62,7 +62,7 @@ grails.project.dependency.resolution = {
 
         runtime 'org.javassist:javassist:3.16.1-GA'
 
-        compile 'org.transmartproject:transmart-core-api:1.2.5-Beta-SNAPSHOT'
+        compile 'org.transmartproject:transmart-core-api:16.1'
 
         compile 'antlr:antlr:2.7.7'
         compile 'net.sf.opencsv:opencsv:2.3'
@@ -137,30 +137,30 @@ grails.project.dependency.resolution = {
         compile ":codenarc:0.21"
 
         if (!dm) {
-            compile ':rdc-rmodules:1.2.5-Beta-SNAPSHOT'
-            runtime ':transmart-core:1.2.5-Beta-SNAPSHOT'
-            compile ':transmart-gwas:1.2.5-Beta-SNAPSHOT'
+            compile ':rdc-rmodules:16.1'
+            runtime ':transmart-core:16.1'
+            compile ':transmart-gwas:16.1'
             //// already included in transmart-gwas
-            //compile ':transmart-legacy-db:1.2.5-Beta-SNAPSHOT'
+            //compile ':transmart-legacy-db:16.1'
             //// already included in transmart-gwas
-            //compile ':folder-management:1.2.5-Beta-SNAPSHOT'
+            //compile ':folder-management:16.1'
             //// already included in transmart-gwas, folder-management
-            //compile ':search-domain:1.2.5-Beta-SNAPSHOT'
+            //compile ':search-domain:16.1'
             //// already included in search-domain, transmart-gwas,
             //                       folder-management
-            //compile ':biomart-domain:1.2.5-Beta-SNAPSHOT'
+            //compile ':biomart-domain:16.1'
             //// already included in biomart-domain
-            //compile ':transmart-java:1.2.5-Beta-SNAPSHOT'
+            //compile ':transmart-java:16.1'
             // -- Genome Browser plugin --
             //   to inclue the plugin, uncomment the following two lines
             //   and see related comment block above 
-            // runtime ':dalliance-plugin:0.2-SNAPSHOT'
-            // runtime ':transmart-mydas:0.1-SNAPSHOT'
-            runtime ':transmart-rest-api:1.2.5-Beta-SNAPSHOT'
-            runtime ':blend4j-plugin:1.2.5-Beta-SNAPSHOT'
-            runtime ':transmart-metacore-plugin:1.2.5-Beta-SNAPSHOT'
+            // runtime ':dalliance-plugin:unknown'
+            // runtime ':transmart-mydas:unknown'
+            runtime ':transmart-rest-api:16.1'
+            runtime ':blend4j-plugin:16.1'
+            runtime ':transmart-metacore-plugin:16.1'
 
-            test ':transmart-core-db-tests:1.2.5-Beta-SNAPSHOT'
+            test ':transmart-core-db-tests:16.1'
         } else {
             dm.internalDependencies delegate
         }
