0a12e6c2d693bd8b4e0134c092aa5cc68a89d0b5
Ruslan Forostianov <ruslan@thehyve.nl>
2016-08-09 19:54:35 +0200

    Upgrade version of rest client.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -122,7 +122,7 @@ grails.project.dependency.resolution = {
         build ':tomcat:7.0.54'
 
         compile ':hibernate:3.6.10.19'
-        compile ':rest-client-builder:2.0.1'
+        compile ':rest-client-builder:2.1.1'
         compile ':cache-ehcache:1.0.5'
         compile ':quartz:1.0-RC2'
         // Not compatible with spring security 3.2 yet
