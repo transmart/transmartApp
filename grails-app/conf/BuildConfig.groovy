ec5dc20922589d4a3107a757bbb7b448248a58d5
dennyverbeeck <dennyverbeeck@gmail.com>
2016-09-01 17:34:21 +0200

    Add custom folder-management and transmart-gwas plugin locations

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -34,6 +34,8 @@ if (dmClass) {
     dm = dmClass.newInstance()
 }
 
+grails.plugin.location.'folder-management' = '../folder-management-plugin'
+grails.plugin.location.'transmart-gwas' = '../transmart-gwas-plugin'
 grails.plugin.location.'transmart-core' = '../transmart-core-db'
 grails.plugin.location.'transmart-core-db-tests' = '../transmart-core-db/transmart-core-db-tests/'
 
@@ -145,7 +147,7 @@ grails.project.dependency.resolution = {
         if (!dm) {
             compile ':rdc-rmodules:16.2-SNAPSHOT'
             //runtime ':transmart-core:16.2-SNAPSHOT'
-            compile ':transmart-gwas:16.2-SNAPSHOT'
+            //compile ':transmart-gwas:16.2-SNAPSHOT'
             //// already included in transmart-gwas
             //compile ':transmart-legacy-db:16.2-SNAPSHOT'
             //// already included in transmart-gwas
