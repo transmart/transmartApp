2b4edd6488a32d2708738277134d01b612811f90
Peter Rice <p.rice@imperial.ac.uk>
2017-11-23 09:30:12 +0000

    minor typo in log debug message

--- grails-app/services/org/transmartproject/security/BruteForceLoginLockService.groovy
+++ grails-app/services/org/transmartproject/security/BruteForceLoginLockService.groovy
@@ -50,7 +50,7 @@ class BruteForceLoginLockService {
      */
     def loginSuccess(String login) {
         if (!isLocked(login)) {
-            log.debug "successfull login for ${login}"
+            log.debug "successful login for ${login}"
             failedAttempts.invalidate(login)
         }
     }
